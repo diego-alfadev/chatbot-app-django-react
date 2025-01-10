0cc957b571e2bcef5332d1c6fdefbd2ce7c0bcc1
DiegoAlfa7 <diegoalfa98@gmail.com>
2025-01-10 21:16:30 +0100

    Remove unused test API endpoint from Django backend

--- backend/api/routes/test.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def hello_world(request):
-    return Response({
-        "data": "Hello world from django backend!"
-    })
