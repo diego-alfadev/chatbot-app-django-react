6e7e63fdcbf93da346060a66323e5bd79cf4789a
DiegoAlfa7 <diegoalfa98@gmail.com>
2025-01-13 02:25:27 +0100

    Configure dayjs for relative time in Spanish; update App component layout

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import SideBar from './sidebar/SideBar';
 import Chat from './chat/Chat';
 
+// Configure dayjs to use relative time in Spanish
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+dayjs().locale('es')
+
 function App() {
   return (
-    <div className="w-full h-full bg-background flex flex-row border-2 border-blue-400">
+    <div className="w-full h-full bg-background flex flex-row">
       <SideBar />
       <Chat/>
     </div>
